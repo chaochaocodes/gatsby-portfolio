b109e5ea3a11ae9151f37a4afb1df85383744903
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-07-30 14:43:20 -0400

    Subject Blog template page Solution dynamically created pages with title slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,29 @@
-// const path = require("path")
+const path = require("path")
 
-// // create pages dynamically
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   const result = await graphql(`
-//     {
-//       blogs: allStrapiBlogs {
-//         nodes {
-//           slug
-//         }
-//       }
-//     }
-//   `)
+// create pages dynamically
+// Node.js file, need to export createPages
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      blogs: allStrapiBlogs {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
 
-//   result.data.blogs.nodes.forEach(blog => {
-//     createPage({
-//       path: `/blogs/${blog.slug}`,
-//       component: path.resolve(`src/templates/blog-template.js`),
-//       context: {
-//         slug: blog.slug,
-//       },
-//     })
-//   })
-// }
+  // create a page forEach blog entry
+  // page context: pass in variable slug, value of blog.slug 
+  // => use variable in blog-template.js
+  result.data.blogs.nodes.forEach(blog => {
+    createPage({
+      path: `/blogs/${blog.slug}`,
+      component: path.resolve(`src/templates/blog-template.js`),
+      context: {
+        slug: blog.slug,
+      },
+    })
+  })
+}
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -7,13 +7,14 @@ const ComponentName = () => {
   return <h2>blog post template</h2>
 }
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
-
-//     }
-//   }
-// `
+// query content info forEach specfic blog post, using unique slug
+// set slug equal to slug variable from page context (gatsby-node.js)
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
+    }
+  }
+`
 
 export default ComponentName
