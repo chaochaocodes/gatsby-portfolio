cd1c8c97420815784fd03ae51c60b57273071194
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-08-05 17:26:54 -0400

    Subject update comments

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
-// Sidebar is position fixed, can render in any order
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 
-// Layout wraps all components, and includes: Navbar, Sidebar, Footer
+// Layout wraps all components
 // Toggle sidebar function passed to navbar and sidebar
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,10 +12,11 @@ const Layout = ({ children }) => {
     setIsOpen(!isOpen)
   }
 
+  // Sidebar is position fixed, can render in any order
   return (
     <>
       <Navbar toggleSidebar={toggleSidebar} />
-      {/* sidebar needs to know it's state! */}
+      {/* sidebar needs to know it's own state! */}
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer/>
